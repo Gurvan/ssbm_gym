fc87f506f0ca3338e0714614fd0060eb78f28009
Gurvan <gurvanpriem@gmail.com>
2019-10-01 20:49:32 +0200

    made installable package

--- test.sh
+++
@@ -1,6 +0,0 @@
-python3 ../dolphin.py --exe dolphin-emu --gfx OGL --audio Pulse \
-                   --speed 1 \
-                   --iso_path "/home/gurvan/ISOs/Super Smash Bros. Melee (USA) (En,Ja) (v1.02).iso" \
-                   --player1 cpu --player2 cpu\
-                   --gui \
-                   --user "/home/gurvan/Dev/ssbm_gym/data/dolphin_conf"
