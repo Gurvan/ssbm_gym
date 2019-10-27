61d5d922f48081087c127140e8eac1ad7232c822
Gurvan <gurvanpriem@gmail.com>
2019-10-27 11:50:07 +0100

    added selfplay exampleé

--- example/main_selfplay.py
+++ example/main_selfplay.py
@@ -2,9 +2,9 @@
 This is an example of environment definition and execution of the program.
 Look inide envs.py to see how to create your own environment.
 You can find how to create an action space inside ssbm_gym/spaces.py (MinimalActionSpace).
-In this code player2 is set as "human", so you can play with the bot if you have a gamecube adapter and a controller in port 2.
+In this code player2 is set as "ai", to give an example of self play interfacing.
 """
-from envs import GoHighEnv
+from envs import SelfPlayEnv
 import atexit
 import platform
 import random
@@ -13,7 +13,7 @@ import time
 options = dict(
     render=True,
     player1='ai',
-    player2='human',
+    player2='ai',
     char1='peach',
     char2='falcon',
     stage='battlefield',
@@ -22,13 +22,13 @@ if platform.system() == 'Windows':
     options['windows'] = True
 
 
-env = GoHighEnv(frame_limit=3600, options=options)
+env = SelfPlayEnv(frame_limit=3600, options=options)
 obs = env.reset()
 atexit.register(env.close)
 
 r = 0
 while True:
-    action = random.randint(0, env.action_space.n - 1)
+    action = tuple([random.randint(0, env.action_space.n - 1) for _ in range(2)])
     obs, reward, done, infos = env.step(action)
     r += reward
     if env.obs.frame % 60 == 0:
